1dd741a6d729db0b77afb04ded598b5dc767221c
= <=>
2021-07-28 15:00:41 +0700

    remove unused

--- bin/checker.dart
+++ bin/checker.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
-
 import 'package:checker/src/data/dependencies_data_source.dart';
 import 'package:checker/src/data/package_license_data_source.dart';
 import 'package:checker/src/data/packages_data_source.dart';
-import 'package:path/path.dart';
 
 void main(List<String> arguments) {
   final dependenciesDataSource = DependenciesDataSource.fromPubspec();
